bbc6c3c84b5cb862e7ed15f38ac57842817efe28
Novali91 <joeyd@firetruk.ca>
2025-06-21 12:58:55 -0700

    Fixed weird player movement priority bug

--- 01_Source/00_Player/player.gd
+++ 01_Source/00_Player/player.gd
@@ -1,17 +1,30 @@
 extends CharacterBody2D
 
 
-@export var top_speed: float = 400
-@export var acceleration: float = 1000
-@export var idle_friction: float = 800
+@export var top_speed: float = 250
+@export var acceleration: float = 700
+@export var idle_friction: float = 600
 # For handling priority. -1 means left/up, 1 means right/down, 0 means idle
 var most_recent_press: Vector2 = Vector2(0, 0)
+var test: bool = false
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
 	# Movement
+	var movement_vector: Vector2 = get_movement_vector()
+	
+	if movement_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(movement_vector * top_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, idle_friction * delta)
+	position += velocity * delta
+	pass
+
+
+func get_movement_vector() -> Vector2:
+	# Most recent key press overrides movement
 	if Input.is_action_just_pressed("left") && most_recent_press.x >= 0:
 		most_recent_press.x = -1
 	elif Input.is_action_just_pressed("right") && most_recent_press.x <= 0:
@@ -22,7 +35,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("left") && Input.is_action_pressed("right"):
 		most_recent_press.x = 1
 	elif Input.is_action_just_released("right") && Input.is_action_pressed("left"):
-		most_recent_press.y = -1
+		most_recent_press.x = -1
 	
 	if Input.is_action_just_pressed("up") && most_recent_press.y >= 0:
 		most_recent_press.y = -1
@@ -31,16 +44,11 @@ func _physics_process(delta: float) -> void:
 	elif !Input.is_action_pressed("up") && !Input.is_action_pressed("down"):
 		most_recent_press.y = 0
 	
+	# If a key is released, resume the previous direction
 	if Input.is_action_just_released("up") && Input.is_action_pressed("down"):
-		most_recent_press.x = 1
+		most_recent_press.y = 1
 	elif Input.is_action_just_released("down") && Input.is_action_pressed("up"):
 		most_recent_press.y = -1
 	
-	var movement_vector: Vector2 = most_recent_press.normalized()
+	return most_recent_press.normalized()
 	
-	if movement_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(movement_vector * top_speed, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, idle_friction * delta)
-	position += velocity * delta
-	pass
