e0cf98d6d2065fc079c051c98e73134db4af7ca5
Novali91 <joeyd@firetruk.ca>
2025-06-21 11:40:43 -0700

    Basic player movement

--- 01_Source/00_Player/player.gd
+++ 01_Source/00_Player/player.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
 
-@export var top_speed: float = 50
-
+@export var top_speed: float = 400
+@export var acceleration: float = 700
+@export var idle_friction: float = 800
 
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
+	# Movement
 	var movement_vector: Vector2 = Input.get_vector("left", "right", "up", "down")
+	
+	if movement_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(movement_vector * top_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, idle_friction * delta)
+	position += velocity * delta
 	pass
