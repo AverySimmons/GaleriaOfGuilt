0812e31a986f832ef7137b5436d0615206296dc4
Novali91 <joeyd@firetruk.ca>
2025-06-21 15:08:45 -0700

    Started main attack and blood bar functionality for player

---
+++ 01_Source/00_Player/blood_swipe.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func initiate_attack() -> void:
+	# Animation player stuff
+	# Deal dmg
+	# Gain blood for each enemy hit and killed
+	return
+
+func attack_not_ready() -> void:
+	return
