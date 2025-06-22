2c64bbd84d07a009d8ee7924fa6461dbaa26ca29
Novali91 <joeyd@firetruk.ca>
2025-06-22 11:21:36 -0700

    Updated bite

--- 01_Source/00_Player/blood_swipe.gd
+++ 01_Source/00_Player/blood_swipe.gd
@@ -2,13 +2,14 @@ extends Area2D
 
 @onready var parent: CharacterBody2D = get_parent()
 var is_active: bool = false
-var active_time: float = 0.6
+var active_time: float = 0.5
 var active_timer: float
 var hit_enemies: Dictionary = {}
 var enemies_just_entered: Array
 var damage: float = 20
-var attack_slowdown: float = 0.3
+var attack_slowdown: float = 0.4
 var attack_slowdown_actual: float = 1.0
+var flinch_amount: float = 0.2
 
 func _ready() -> void:
 	connect("area_entered", Callable(self, "_on_area_entered"))
@@ -22,11 +23,13 @@ func _physics_process(delta: float) -> void:
 			continue
 		parent.blood_bar += parent.bb_hit
 		hit_enemies[enemy] = null
+		enemy.take_damage(damage, flinch_amount)
 		# Enemy take damage thing
 	enemies_just_entered.clear()
 	
 	active_timer = move_toward(active_timer, 0, delta)
 	if active_timer <= 0:
+		monitoring = false
 		is_active = false
 		hit_enemies.clear()
 		attack_slowdown_actual = 1.0
@@ -34,14 +37,19 @@ func _physics_process(delta: float) -> void:
 
 func initiate_attack() -> void:
 	# Animations:
-	# Slight screen shake?
 	# Enemies flash red - In enemy take damage thing
 	# Animation player stuff
 	# Audio
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	var direction: Vector2 = mouse_pos - global_position
+	var angle: float = direction.angle()
+	rotation = angle
+	# Make player face direction of swing
+	
 	active_timer = active_time
 	attack_slowdown_actual = attack_slowdown
 	is_active = true
-	
+	monitoring = true
 	if has_overlapping_areas():
 		parent.dealt_damage_took_damage = true
 	
@@ -51,6 +59,7 @@ func initiate_attack() -> void:
 			continue
 		parent.blood_bar += parent.bb_hit
 		hit_enemies[enemy] = null
+		enemy.take_damage(damage, flinch_amount)
 	# Functions:
 	# Deal dmg
 	# Gain blood for each enemy hit and killed
--- 03_Components/00_Special_Abilities/bite.gd
+++ 03_Components/00_Special_Abilities/bite.gd
@@ -7,14 +7,16 @@ var active_timer: float
 var hit_enemies: Dictionary = {}
 var enemies_just_entered: Array
 var blood_gain_multiplier: float = 2.5
-var slowdown: float = 0.4
+var attack_slowdown: float = 0.2
+var attack_slowdown_actual: float = 1.0
+var flinch_amount: float = 0.3
 
 
 func _ready() -> void:
 	super._ready()
 	damage = 50
 	cooldown = 4
-	chargeup = 0.4
+	chargeup = 0.3
 	
 	connect("area_entered", Callable(self, "_on_area_entered"))
 	pass
@@ -27,7 +29,7 @@ func _physics_process(delta: float) -> void:
 			continue
 		parent.blood_bar += parent.bb_hit * blood_gain_multiplier
 		hit_enemies[enemy] = null
-		# Enemy take damage thing
+		enemy.take_damage(damage, flinch_amount)
 	enemies_just_entered.clear()
 	
 	active_timer = move_toward(active_timer, 0, delta)
@@ -50,6 +52,7 @@ func use_ability(mouse_position: Vector2) -> void:
 			continue
 		parent.blood_bar += parent.bb_hit * blood_gain_multiplier
 		hit_enemies[enemy] = null
+		enemy.take_damage(damage, flinch_amount)
 	return
 
 func _on_area_entered(area: Area2D) -> void:
