f5ec195090ce0409e164fe9676c89c76f516b75f
Novali91 <joeyd@firetruk.ca>
2025-06-21 17:22:51 -0700

    Humble blood swipe walks in (humble players changes how she takes damage))

--- 01_Source/00_Player/blood_swipe.gd
+++ 01_Source/00_Player/blood_swipe.gd
@@ -1,8 +1,21 @@
 extends Area2D
 
+@onready var parent: CharacterBody2D = get_parent()
 func initiate_attack() -> void:
-	
+	# Animations:
+	# Slight screen shake?
+	# Enemies flash red
 	# Animation player stuff
+	# Audio
+	if has_overlapping_areas():
+		parent.dealt_damage_took_damage = true
+	
+	var enemies_hit = get_overlapping_areas()
+	for enemy in enemies_hit:
+		if enemy is not Enemy:
+			continue
+		parent.blood_bar += parent.bb_hit
+	# Functions:
 	# Deal dmg
 	# Gain blood for each enemy hit and killed
 	return
