b2240dfd168856c396219c2e670ada41a92ba53b
Sundered7 <brian@froods.ca>
2025-06-24 16:51:34 -0700

    worm projectile

--- 01_Source/01_Combat/Enemies/Projectiles/sand_projectile.gd
+++ 01_Source/01_Combat/Enemies/Projectiles/sand_projectile.gd
@@ -1,4 +1,9 @@
 extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
-	move_and_slide()
+	var collided = move_and_slide()
+	if collided:
+		die()
+		
+func die() -> void:
+	queue_free()
