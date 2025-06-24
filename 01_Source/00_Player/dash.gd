b22717bd4b9321d892e8d0ffafa5bb3d46a16b83
Novali91 <joeyd@firetruk.ca>
2025-06-24 13:42:06 -0700

    Dash stuff

---
+++ 01_Source/00_Player/dash.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@onready var parent = get_parent()
+var dash_speed_mult: float = 1.0
+
+func start_dash(speed: float, distance: float, direction: Vector2) -> void:
+	speed = speed*dash_speed_mult
+	parent.velocity = speed*direction
+	var time = distance/speed
+	print(parent.using_attack_or_special_or_dash)
+	parent.is_dashing = true
+	parent.using_attack_or_special_or_dash = true
+	await get_tree().create_timer(time).timeout
+	print("JAHSHUHDH")
+	end_dash()
+	return
+
+func end_dash() -> void:
+	parent.velocity = Vector2.ZERO
+	parent.is_dashing = false
+	parent.using_attack_or_special_or_dash = false
+	return
