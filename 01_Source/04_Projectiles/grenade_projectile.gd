bae6fa12d65154be818ee868c97c9562ac31bf3f
Novali91 <joeyd@firetruk.ca>
2025-06-23 22:53:19 -0700

    Player animation speed relates to speed + Grenade stuff

--- 01_Source/04_Projectiles/grenade_projectile.gd
+++ 01_Source/04_Projectiles/grenade_projectile.gd
@@ -1,1 +1,31 @@
 extends Area2D
+
+var velocity: Vector2
+var explosion_time: float = 1.0
+var timer_until_explosion: float = 2.0
+var target_location: Vector2
+
+# Z variable for grenade arc
+var z: float
+var z_speed: float
+var z_gravity: float = -200
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
+	
+	# For the arc
+	z_speed -= z_gravity * delta
+	z += z_speed * delta
+	$Sprite2D.position.y = z
+	if z <= 0:
+		explode()
+	pass
+
+func get_thrown() -> void:
+	return
+
+func explode() -> void:
+	return
