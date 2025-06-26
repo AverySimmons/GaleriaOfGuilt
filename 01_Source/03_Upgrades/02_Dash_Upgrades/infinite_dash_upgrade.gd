e94376f5023982a20b91682fe4caafafcada95ab
Novali91 <joeyd@firetruk.ca>
2025-06-25 22:01:56 -0700

    Created dash upgrade for infinite dash + started marking dash upgrade

---
+++ 01_Source/03_Upgrades/02_Dash_Upgrades/infinite_dash_upgrade.gd
@@ -0,0 +1,15 @@
+extends Upgrade
+
+func _init() -> void:
+	upgrade_name = "Reckless Restlessness"
+	upgrade_description = "Dashing has almost no cooldown but costs blood. Yippee!"
+	icon = preload("res://00_Assets/00_Sprites/upgrade_icons/boots_super.png")
+	upgrade_number = UpgradeData.DASH_INFINITE
+	upgrade_scene = self
+	pass
+
+func choose_upgrade() -> void:
+	super.choose_upgrade()
+	GameData.player.dash_blood_cost += 10
+	GameData.player.dash_cd = 0.5
+	return
