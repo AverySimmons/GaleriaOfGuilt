3cb5d47143915cc5cad41a6c68684c729ec50014
Novali91 <joeyd@firetruk.ca>
2025-06-26 20:22:12 -0700

    Implemented the new upgrade icons

--- 01_Source/03_Upgrades/00_Swipe_Upgrades/costs_blood_more_dmg_swipe.gd
+++ 01_Source/03_Upgrades/00_Swipe_Upgrades/costs_blood_more_dmg_swipe.gd
@@ -3,7 +3,7 @@ extends Upgrade
 func _init() -> void:
 	upgrade_name = "Blood-Infused Claws"
 	upgrade_description = "Your swipe attack costs some blood but deals more damage."
-	icon = preload("res://00_Assets/00_Sprites/upgrade_icons/slash_blood.png")
+	icon = preload("res://00_Assets/00_Sprites/upgrade_icons/slash_glowing.png")
 	upgrade_number = UpgradeData.COSTS_BLOOD_MORE_DMG
 	upgrade_scene = self
 	pass
--- 01_Source/03_Upgrades/02_Dash_Upgrades/mark_dash_upgrade.gd
+++ 01_Source/03_Upgrades/02_Dash_Upgrades/mark_dash_upgrade.gd
@@ -3,7 +3,7 @@ extends Upgrade
 func _init() -> void:
 	upgrade_name = "Easy Prey" # (The Meat King's Party!) That's what they call Jai (Jai said this) (mmm... hemomancer)
 	upgrade_description = "Hitting an enemy with your dash marks them. Marked enemies provide more blood when hit."
-	icon = preload("res://00_Assets/00_Sprites/upgrade_icons/blood_plus.png")
+	icon = preload("res://00_Assets/00_Sprites/upgrade_icons/dash_slashing.png")
 	upgrade_number = UpgradeData.MARK_DASH
 	upgrade_scene = self
 	pass
