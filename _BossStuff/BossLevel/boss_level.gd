98b967ce54f148650d9c898a20814c77cff9049b
Avery Simmons <eno116116@gmail.com>
2025-07-07 16:07:37 -0700

    boss stuff

--- _BossStuff/BossLevel/boss_level.gd
+++ _BossStuff/BossLevel/boss_level.gd
@@ -1,8 +1,20 @@
 extends Node2D
 
 @onready var entities: Node2D = $Entities
+@onready var attack_indicators: Node2D = $AttackIndicators
 
+@onready var boss_lightning_module: BossLightningModule = $BossLightningModule
+
+var timer = 10
 
 func _ready() -> void:
+	boss_lightning_module.indicator_node = attack_indicators
 	GameData.player.global_position = $PlayerSpawn.global_position
 	entities.add_child(GameData.player)
+	boss_lightning_module.add_lightning(2)
+
+func _physics_process(delta: float) -> void:
+	timer -= delta
+	if timer < 0:
+		timer = 10
+		boss_lightning_module.activate(3)
--- _BossStuff/LightningModule/lightning_module.gd
+++ _BossStuff/LightningModule/lightning_module.gd
@@ -1,53 +1,49 @@
+class_name BossLightningModule
 extends Node
 
 signal finished()
 
+const level_radius = 360
+
 var blood_lightning_scene = preload("res://_BossStuff/LightningModule/blood_lightning.tscn")
 var indicator_node: Node2D
-
-var spawning_window = 3
-var animation_window = 2
-
-var spawning_timer = 0
-var animation_timer = 0
 
 var animation_finished = false
 
 var spawner_num = 3
-var lightning_acc = 2000
+var base_lightning_acc = 1000
+var speed_up_lightning_acc = 3000
+var acc = base_lightning_acc
 var spawners: Array[BossLightningSpawner]
 
-func activate() -> void:
-	spawning_timer = spawning_window
-	animation_timer = animation_window
-	animation_finished = false
+func add_lightning(num: int) -> void:
+	var start_angle = randf_range(0, TAU)
 	
-	spawners = []
-	for i in spawner_num:
+	for i in num:
 		var new_spawner = BossLightningSpawner.new()
 		new_spawner.velocity = Vector2.ZERO
-		new_spawner.pos = Vector2.ZERO
+		new_spawner.pos = Vector2.from_angle(start_angle) * level_radius
+		spawners.push_back(new_spawner)
+		start_angle += TAU / float(num)
+
+func activate(time: float) -> void:
+	acc = speed_up_lightning_acc
+	await get_tree().create_timer(time,false)
+	acc = base_lightning_acc
+	finished.emit()
 
 func _physics_process(delta: float) -> void:
-	if spawning_timer < 0:
-		return
-	
-	if animation_timer < 0 and not animation_finished:
-		finished.emit()
-		animation_finished = true
-	
-	spawning_timer -= delta
-	animation_timer -= delta
-	
 	for s in spawners:
 		var dir = s.pos.direction_to(GameData.player.global_position)
-		s.velocity += dir * lightning_acc * delta
+		s.velocity += dir * acc * delta
 		s.pos += s.velocity * delta
 		
-		if s.pos.distance_to(s.last_spawn_position) > 40:
+		if s.pos.distance_to(s.last_spawn_position) > 45:
 			spawn_lightning(s.pos)
+			s.last_spawn_position = s.pos
 
 func spawn_lightning(pos: Vector2) -> void:
+	print("!")
 	var new_lightning = blood_lightning_scene.instantiate()
 	new_lightning.global_position = pos
 	indicator_node.add_child(new_lightning)
