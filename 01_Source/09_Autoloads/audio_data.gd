8e0cd714141f4b79f754e369518baecbc134d9cb
Sardet <partycrasherer@gmail.com>
2025-07-09 19:14:05 -0700

    stuff

--- 01_Source/09_Autoloads/audio_data.gd
+++ 01_Source/09_Autoloads/audio_data.gd
@@ -2,6 +2,8 @@ extends Node
 
 const MAX_SOUNDFX_AMT: int = 16
 const INDIVIDUAL_MAX_AMT: int = 4
+const LIGHTNING_MAX: int = 10
+const TORNADO_BULLET_MAX: int = 16
 
 var cur_total: int = 0
 var locust_atk_amt: int = 0
@@ -19,6 +21,10 @@ var enemy_death_amt: int = 0
 var explosion_sound: int = 0
 var player_hit: int
 var upgrade_amt: int
+var lightning_amt: int
+var lightning_alt: bool = true
+var tornado_amt: int
+var tornado_bullets: int
 
 func play_sound(sound_name: String, sound) -> void:
 	if sound == null:
@@ -150,4 +156,34 @@ func play_sound(sound_name: String, sound) -> void:
 				await get_tree().create_timer(sound.stream.get_stream(0).get_length(), false).timeout
 				cur_total -= 1
 				upgrade_amt -= 1
+				
+		"lightning":
+			if lightning_amt < LIGHTNING_MAX:
+				lightning_alt = !lightning_alt
+				if lightning_alt:
+					sound.play()
+					cur_total += 1
+					lightning_amt += 1
+					await get_tree().create_timer(sound.stream.get_stream(0).get_length(), false).timeout
+					cur_total -= 1
+					lightning_amt -= 1
+					
+		"tornado_spawn":
+			if tornado_amt < INDIVIDUAL_MAX_AMT:
+				sound.play()
+				cur_total += 1
+				tornado_amt += 1
+				await get_tree().create_timer(sound.stream.get_stream(0).get_length(), false).timeout
+				cur_total -= 1
+				tornado_amt -= 1
+				
+		"tornado_shoot":
+			if tornado_bullets < TORNADO_BULLET_MAX:
+				sound.play()
+				cur_total += 1
+				tornado_bullets += 1
+				await get_tree().create_timer(sound.stream.get_stream(0).get_length(), false).timeout
+				cur_total -= 1
+				tornado_bullets -= 1
+				
 	return
